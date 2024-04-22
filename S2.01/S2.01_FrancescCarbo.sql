a877c89fd41b0c2d21d7b599dacf358dce31e0d4
kiku-x <108832679+kiku-x@users.noreply.github.com>
2024-04-22 12:02:25 +0200

    Update S2.01_FrancescCarbo.sql

--- S2.01/S2.01_FrancescCarbo.sql
+++ S2.01/S2.01_FrancescCarbo.sql
@@ -2,27 +2,31 @@
 Exercici 1: Mostra totes les transaccions realitzades per empreses d'Alemanya.*/
 SELECT *
 FROM transaction
-LEFT JOIN company -- Fusionem la taula company amb la taula transaction.
-ON company_id = company.id
-WHERE company.country = (
-	SELECT company.country
-	FROM company 
-	WHERE company.country = "Germany" -- Apliquem el filtre on només volem que es mostrin les transaccions que permtanyes a empreses de Germany.
-    LIMIT 1
-    );
-
-
+WHERE transaction.company_id IN (SELECT company.id
+                                 FROM company
+                                 WHERE company.country = 'Germany');
+                                 
 
 /*Exercici 2: Màrqueting està preparant alguns informes de tancaments de gestió, et demanen que els passis un llistat de les
 empreses que han realitzat transaccions per una suma superior a la mitjana de totes les transaccions.*/
 SELECT DISTINCT *
 FROM company
-LEFT JOIN transaction
-ON company.id = company_id
+JOIN transaction
+ON company.id = transaction.company_id
 WHERE amount > ( -- Fem un filtre perque només mostri els amounts que siguin mes grans que el numero calculat dins al subquery.
 	SELECT AVG(transaction.amount) -- Creem una subquery per calcular la mitjana de totes les transaccions.
 	FROM transaction
     );
+
+SELECT DISTINCT *
+FROM company
+WHERE company.id IN (
+	SELECT company.id
+	FROM transaction
+	WHERE transaction.amount > (
+		SELECT AVG(transaction.amount)
+		FROM transaction ));
+
 
 
 /*Exercici 3: El departament de comptabilitat va perdre la informació de les transaccions realitzades per una empresa,
@@ -36,56 +40,48 @@ FROM (
 ) AS subquery
 WHERE subquery.company_name LIKE "c%"; -- Creem un filtre amb una wildcard con diem que mostri totes les empreses que el nom comenci per C.
 
-
 -- Tambe podria ajudar-los proporcionant les transaccions de les empreses que comencen amb la lletra C.
 SELECT *
-FROM (
-    SELECT company.id AS company_id, company.company_name, transaction.id AS transaction_id, transaction.company_id AS transaction_company_id
-    FROM company
-    LEFT JOIN transaction
-    ON company.id = transaction.company_id
-) AS subquery
-WHERE subquery.company_name LIKE "c%"; -- Creem un filtre amb una wildcard con diem que mostri totes les empreses que el nom comenci per C.
+FROM transaction
+WHERE transaction.company_id IN (SELECT company.id
+FROM company
+WHERE company_name LIKE "c%");
+
 
 
 -- Exercici 4: Van eliminar del sistema les empreses que no tenen transaccions registrades, lliura el llistat d'aquestes empreses.
-SELECT *
-FROM (
-    SELECT *
-    FROM company
-    WHERE company_name LIKE "c%"
-) AS filtered_company
-LEFT JOIN transaction
-ON filtered_company.id = company_id; -- Mostra les empreses que al fer el join, no tinguin info de cap transaccio.
 
+SELECT DISTINCT(company.company_name)
+FROM company
+WHERE company.id IN (
+	SELECT transaction.company_id
+	FROM transaction
+	WHERE transaction.company_id IS NULL);
 
 /*NIVELL 2
 Exercici 1: En la teva empresa, es planteja un nou projecte per a llançar algunes campanyes publicitàries per a fer competència a la companyia senar institute.
 Per a això, et demanen la llista de totes les transaccions realitzades per empreses que estan situades en el mateix país que aquesta companyia.*/
 SELECT *
 FROM transaction
-LEFT JOIN company
-ON company_id = company.id
-WHERE country = ( -- Filtre per mostrar els resultats on el pais sigui el mateix al obtingut per la subquery.
-	SELECT country -- Subquery per saber mostrar el pais de l'empresa Amet Institute.
-	FROM company
-	WHERE company_name = "Non Institute"
-	);
-
+WHERE transaction.company_id IN (
+    SELECT company.id
+    FROM company
+    WHERE company.country = (
+        SELECT company.country
+        FROM company
+        WHERE company_name = 'Non institute'
+    )
+);
 
 -- Exercici 2: El departament de comptabilitat necessita que trobis l'empresa que ha realitzat la transacció de major suma en la base de dades.
-SELECT company_transaction.company_id, company_transaction.company_name, company_transaction.transaction_id, company_transaction.transaction_company_id
-FROM (
-    SELECT company.id AS company_id, company.company_name, transaction.id AS transaction_id, transaction.company_id AS transaction_company_id
-    FROM company
-    JOIN transaction
-    ON company.id = company_id
-    ORDER BY amount DESC
-) AS company_transaction
+SELECT company_name
+FROM company
+WHERE company.id IN (
+	SELECT company_id
+	FROM transaction
+	ORDER BY amount DESC
+    )
 LIMIT 1;
-
- -- Fem que nomes es mostri el primer resultat. Aixi obtenim el nom de l'empresa que ha fet la transaccio amb despesa mes gran.
-
 
 /*NIVELL 3
 Exercici 1: S'estan establint els objectius de l'empresa per al següent trimestre, per la qual cosa necessiten una base sòlida per a avaluar el rendiment
